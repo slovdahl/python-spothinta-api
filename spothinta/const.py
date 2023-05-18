1abbeeb7f45c4fc83865b969922702f55b0ca321
Sebastian Lövdahl <slovdahl@hibox.fi>
2023-05-18 21:27:55 +0300

    Support for getting prices for a specific region

--- spothinta/const.py
+++ spothinta/const.py
@@ -1,4 +1,26 @@
 """Constants for SpotHinta API client."""
+from enum import Enum, unique
 from typing import Final
 
 API_HOST: Final = "api.spot-hinta.fi"
+
+
+@unique
+class Region(Enum):
+    """An enumeration of the supported regions."""
+
+    DK1 = 0
+    DK2 = 1
+    FI = 2
+    EE = 3
+    LT = 4
+    LV = 5
+    NO1 = 6
+    NO2 = 7
+    NO3 = 8
+    NO4 = 9
+    NO5 = 10
+    SE1 = 11
+    SE2 = 12
+    SE3 = 13
+    SE4 = 14
